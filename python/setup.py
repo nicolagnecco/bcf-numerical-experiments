336877c7757d9f4bf731eb57bc10410fedf74a88
Nicola Gnecco <nicola.gnecco@gmail.com>
2024-07-30 12:25:31 +0100

    update setup

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="src",
-    packages=find_packages(include=["src", "src.*"]),
+    packages=find_packages(include=["src", "src.*", "configs"]),
     version="0.1.0",
     description="Implementation of stable trees,",
     author="Nicola Gnecco, Sebastian Engelke, Jonas Peters, and Niklas Pfister",
