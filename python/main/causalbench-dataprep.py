e9f96e16a45447dc683c3d9f1dbe4e45b95a20b6
Nicola Gnecco <nicola.gnecco@gmail.com>
2024-07-31 12:03:48 +0100

    prepare data get all genes

--- python/main/causalbench-dataprep.py
+++ python/main/causalbench-dataprep.py
@@ -36,7 +36,7 @@ def is_cause(
 
 
 # %%
-def main(input_path: str, output_path: str):
+def main(input_path: str, output_path: str, all_rows: bool = False):
 
     # %%
     # input_path = "../../data/raw/genes/dataset_k562_filtered.npz"
@@ -51,8 +51,11 @@ def main(input_path: str, output_path: str):
     selected_genes_idx = np.where((obs_data > 0).mean(axis=0) == 1)[0]
     selected_genes_names = dat["var_names"][selected_genes_idx]
 
-    # Rows to extract from dat_filtered and dat["interventions"]
-    row_mask = obs_index | np.isin(dat["interventions"], selected_genes_names)
+    if all_rows:
+        row_mask = dat["interventions"] != "excluded"
+    else:
+        # Rows to extract from dat_filtered and dat["interventions"]
+        row_mask = obs_index | np.isin(dat["interventions"], selected_genes_names)
     # %%
     # Datasets only keeping selected_genes_idx
     X = dat["expression_matrix"][row_mask][:, selected_genes_idx]
@@ -78,7 +81,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output", type=str, required=True, help="Path to output data file"
     )
+    parser.add_argument(
+        "--all_rows",
+        required=False,
+        action="store_true",
+        help="Whether to keep all observations or only the ones corresponding to observational setting + selected genes. Default is False.",
+    )
 
+    # how do I pass a boolean argument from terminal?
     args = parser.parse_args()
 
-    main(args.input, args.output)
+    main(args.input, args.output, args.all_rows)
