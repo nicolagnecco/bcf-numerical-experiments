1e27c6cdffa4fae4051cae8162a90a44180160e7
Nicola Gnecco <nicola.gnecco@gmail.com>
2024-08-08 11:02:26 +0200

    look at results

--- R-code/main/plot-causalbench-analysis.R
+++ R-code/main/plot-causalbench-analysis.R
@@ -1,12 +1,16 @@
 source("main/dependencies.R")
 
-# SETTING: use as training envs genes that are not predictors.
-# TRAIN: Y, X, envs != X -> mod
-# TEST: predict(mod, env = X[j]) for j = 1, ..., p
-# 
+"../results/causalbench-analysis-2/n_preds_3-train_mask_top-confounders_False/20240808-104520//"
+base_folder <- "../results/causalbench-analysis-2"
+experiment <- "n_preds_3-train_mask_top-confounders_False"
+run <- "20240808-104520"
+res_name <- "causalbench-res.csv"
+
+filename <- file.path(base_folder, experiment, run, res_name)
+imagename <- file.path(base_folder, experiment, run, "res.pdf")
 
 res <- read_csv(
-  "../results/causalbench-analysis/n_preds_9-n_trainenv_5-confounders_False/20240806-121807//causalbench-res.csv"
+  filename
 ) %>%
   mutate(algorithm = refactor_methods(algorithm, rev=TRUE)) 
 
@@ -30,19 +34,15 @@ dat2plot_agg <- dat2plot %>%
   summarise(mse = mean(mse))
 
 
-# fix P, increase R -> get classical control function
-# 
-
-
-ggplot(data=dat2plot_agg) +
+gg <- ggplot(data=dat2plot_agg %>% filter(algorithm == algorithm)) +
   geom_line(mapping=aes(x = interv_strength, y = mse, 
                         col = algorithm, size = algorithm, linetype=algorithm))+
-  # geom_line(data=dat2plot, mapping=aes(x = interv_strength, y = mse, col = algorithm,
-  #                                      group = interaction(algorithm, response)),
-  #           alpha = .2) +
   geom_point(aes(x = interv_strength, y = mse, col = algorithm, shape=algorithm), 
              fill = "white", size = 2, stroke = 0.75, alpha = 0.75) +
   scale_color_manual(values = my_colors, guide = guide_legend(reverse = TRUE)) +
   scale_size_manual(values = my_sizes, guide = guide_legend(reverse = TRUE)) +
   scale_linetype_manual(values = my_linetypes, guide = guide_legend(reverse = TRUE)) +
-  scale_shape_manual(values = my_shapes, guide = guide_legend(reverse = TRUE)) 
+  scale_shape_manual(values = my_shapes, guide = guide_legend(reverse = TRUE)); gg
+
+save_myplot(gg, imagename,
+            width = 4, height = 4)
